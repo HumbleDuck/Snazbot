dd03cc4052d662e18f1ba3b5f02a36eb4f524988
Sheemap <jamman98@gmail.com>
2018-03-02 12:55:10 -0700

    Main notification ability

--- includes/common.js
+++ includes/common.js
@@ -33,6 +33,42 @@ exports.sendMsg = function(msg,content,reply,timeout,callback){
     }
 }
 
+exports.sendChannel = function(chan,content,timeout,callback){
+    let sent;
+    let push = true;
+    
+    let all_channels = app.client.channels.array();
+    let channel = '';
+
+    for(let c in all_channels){
+        if(all_channels[c].id == chan){
+            channel = all_channels[c];
+        }
+    }
+
+    if(channel !== ''){
+        channel.send(content).then(message => cb(message));//.then(message => sent=message);//
+    }else{
+        logger.log('error',`Attempted to send message to nonexistent channel! Channel: ${channel.id}`)
+    }
+    
+    if(!isNaN(timeout) && app.autoclean == 'yes'){
+        push = false;
+        sleep(timeout*1000).then(function(){
+            sent.delete().catch(function(){logger.log('warn','Tried to delete a nonexistent message!')});
+        })
+    }
+    function cb(message){
+        if(push){
+            msgs.push(message);
+        }
+
+        if(typeof(callback) !== 'undefined'){
+            callback(message);
+        }
+    }
+}
+
 exports.reset = function(){
 	msgs = [];
 }
